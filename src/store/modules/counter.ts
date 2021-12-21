dff46e9f4b1504ae2c5307b0818c13c34dc60b2e
nikolausliu <18010855044@163.com>
2021-12-21 22:21:31 +0800

    feat: counterStore use vuex-style

--- src/store/modules/counter.ts
+++ src/store/modules/counter.ts
@@ -1,16 +1,12 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
-export const useCounterStore = defineStore('counter', () => {
-  // state
-  const count = ref(0)
-
-  // getters
-  const double = computed(() => count.value * 2)
-
-  // actions
-  function increment() {
-    count.value++
-  }
-
-  return { count, double, increment }
+export const useCounterStore = defineStore('counter', {
+  state: () => ({ count: 0 }),
+  getters: {
+    double: (state) => state.count * 2,
+  },
+  actions: {
+    increment() {
+      this.count++
+    },
+  },
 })
